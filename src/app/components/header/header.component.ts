dac4e0b8013c83739d3947490336e9d95db60b48
alexy <Alexis.Young@uqtr.ca>
2025-04-02 13:30:17 -0400

    Fix mobile

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {NgOptimizedImage} from '@angular/common';
 import {RouterLink} from '@angular/router';
+import {MatSidenav, MatSidenavModule} from '@angular/material/sidenav';
+import {MatListItem} from '@angular/material/list';
 
 @Component({
   selector: 'app-header',
@@ -11,13 +13,14 @@ import {RouterLink} from '@angular/router';
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    NgOptimizedImage,
-    RouterLink
+    RouterLink,
+    MatSidenavModule,
+    MatListItem,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  @ViewChild('sidenav') sidenav!: MatSidenav;
   userProfilPicturePath: string = "/../../assets/Logo.png";
 }
