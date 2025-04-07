fd8595757bd8142895fb55a74b09dfc815419480
alexy <Alexis.Young@uqtr.ca>
2025-04-06 20:39:28 -0400

    404 403

---
+++ src/app/components/error403/error403.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-error403',
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './error403.component.html',
+  styleUrl: './error403.component.css'
+})
+export class Error403Component {
+
+}
